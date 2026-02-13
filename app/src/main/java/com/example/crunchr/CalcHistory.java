acd4da8d38760444f5d892e2754ac4b3416bbe05
Geoffrey0107 <geoffrey.duan@hotmail.com>
2026-02-12 22:49:09 -0500

    Added comments

--- app/src/main/java/com/example/crunchr/CalcHistory.java
+++ app/src/main/java/com/example/crunchr/CalcHistory.java
@@ -2,7 +2,6 @@ package com.example.crunchr;
 
 import java.util.ArrayList;
 public class CalcHistory {
-    // "since the app was launched" → memory only
     public static final ArrayList<String> items = new ArrayList<>();
 
     public static void add(String infix, String resultStr) {
